499594a30a000d7d497aba801fa7d69b0439a54d
Alisher Temirov <altem0510@outlook.com>
2021-06-19 14:49:09 +0300

    fix minority issues; Enable Django admin for UserProfile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,12 +7,12 @@ class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
 
-    def create_user(self, name, email, password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError("User must have an email address")
 
-        email = self.normalize_email(mail)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -23,10 +23,10 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self, email, name, password):
         """Create and save a new superuser"""
 
-        user.self_create_user(email, name, password)
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=seld._db)
+        user.save(using=self._db)
 
         return user
 
